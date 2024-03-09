c38c8e8a369b6ce7ee9ef95a69a766991d676c2f
Valentin Valev <valio.94.v@gmail.com>
2024-03-09 19:23:22 +0200

    Add header component and styles

--- computerShopApp/src/app/app.module.ts
+++ computerShopApp/src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,6 +14,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
     AppRoutingModule
   ],
   providers: [],
