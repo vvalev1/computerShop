2b15204c3d7a6f1d31ffdd5e85fa1b4c76887099
Valentin Valev <valio.94.v@gmail.com>
2024-03-26 01:04:19 +0200

    implements register functionality

--- computerShopApp/src/app/user/register/register.component.ts
+++ computerShopApp/src/app/user/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  public errorMsg: string = '';
+
+  constructor(private userService: UserService, private router: Router) {}
+
+  register(registerForm: NgForm) {
+ 
+    const { name, email, password, retypePassword } = registerForm.value;
+
+    if(email=== '' || password === '' || name === '') {
+      this.errorMsg = 'Please, fill in all the fields!';
+      return;
+
+    } else if(retypePassword !== password) {
+      this.errorMsg = 'Password and Retype password are not equal!';
+      return;
+
+    } else {
+      this.errorMsg = '';
+    }
+
+
+    this.userService.register(name, email, password).subscribe({
+      next: () => {
+        this.router.navigate(['/products']);
+      },
+      error: (e) => {
+        this.errorMsg = e.error.message;
+      }
+    });
+
+  }
 }
