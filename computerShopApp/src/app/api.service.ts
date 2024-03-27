2cf22735790067a1bc671491acd6bbf981f8fc7d
Valentin Valev <valio.94.v@gmail.com>
2024-03-28 00:27:07 +0200

    implement api service and add create product post request

---
+++ computerShopApp/src/app/api.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SERVER_BASE_URL } from './constants';
+import { Product } from './types/product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) { }
+
+  createProduct(productValues: object, owner_id: string | undefined) {
+    const baseUrl = SERVER_BASE_URL;
+    return this.http.post<Product>(`${baseUrl}/data/products`, {...productValues, owner_id});
+  }
+
+}
--- computerShopApp/src/app/products/create-product/create-product.component.ts
+++ computerShopApp/src/app/products/create-product/create-product.component.ts
@@ -1,4 +1,9 @@
+import { HttpParams } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/api.service';
+import { UserService } from 'src/app/user/user.service';
 
 @Component({
   selector: 'app-create-product',
@@ -6,5 +11,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-product.component.css']
 })
 export class CreateProductComponent {
+
+  productTypes: string[] = [
+    "Laptops and Computers",
+    "Components and peripherals",
+    "Monitors",
+    "Servers and storages",
+    "Printers",
+    "Routers",
+
+  ]
+
+  public errorMsgs: string[] = [];
+  private owner_id: string | undefined;
+
+  constructor(private apiService: ApiService, private router: Router, private userService: UserService) {
+    this.owner_id = this.userService.user?._id;
+  }
+
+  createProduct(createForm: NgForm) {
+
+    this.errorMsgs = [];
+    let isError: boolean = false;
+
+    const { name, price, imgUrl, quantity, productType, description } = createForm.value;
+    
+
+    if(name === '' || price === '' || imgUrl === '' 
+      || quantity === '' || productType === '' || description === '') {
+
+        this.errorMsgs.push('Please, fill in all the fields!');
+        isError = true;
+    }
+
+    if(price <= 0) {
+
+      this.errorMsgs.push('"Price" must be positive number!');
+      isError = true;
+    }
+
+    if(quantity <= 0) {
+
+      this.errorMsgs.push('"Quantity" must be positive number!');
+      isError = true;    
+    }
+
+    if(isError) {
+      return;
+    }
+    
+    this.apiService.createProduct(createForm.value, this.owner_id).subscribe({
+      next: () => {
+        console.log(this.owner_id);
+        this.router.navigate(['/products']);
+      },
+      error: (e) => {
+        this.errorMsgs.push(e.error.message);
+      }
+    })
+
+
+  }
 
 }
