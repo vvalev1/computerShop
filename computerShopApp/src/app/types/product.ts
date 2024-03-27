6ca242ed04d0a63817ca032ab8621a61d154ebd3
Valentin Valev <valio.94.v@gmail.com>
2024-03-27 21:21:03 +0200

    implement Product type interface

---
+++ computerShopApp/src/app/types/product.ts
@@ -0,0 +1,10 @@
+export interface Product{
+    _id: string
+    name: string,
+    price: string,
+    imageUrl: string,
+    quantity: number, 
+    productType: string,
+    description: string,
+    owner_id: string
+}
