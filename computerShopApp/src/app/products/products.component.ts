d78ea02695b133a907325a000c5f7cdff038bdc9
Valentin Valev <valio.94.v@gmail.com>
2024-04-03 00:36:27 +0300

    implement filters by  type product

--- computerShopApp/src/app/products/products.component.ts
+++ computerShopApp/src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../types/product';
 import { ApiService } from '../api.service';
 
@@ -10,12 +10,14 @@ import { ApiService } from '../api.service';
 })
 export class ProductsComponent implements OnInit {
 
-  @Input() products: Product[] | null
+  @Input() products: Product[] | undefined
   @Input() isExternalRendered: boolean = false;
 
-  errorMsg = '';
+  @Input() errorMsg: string;
 
-  // products: Product[] | null = [];
+  allProducts: Product[] | undefined = [];
+
+  isActive: boolean = false;
 
   constructor(private apiService: ApiService) {}
 
@@ -23,7 +25,9 @@ export class ProductsComponent implements OnInit {
     if(!this.isExternalRendered) {
       this.apiService.getAllProducts().subscribe({
         next: (products: any) => {
-          this.products = products;
+          this.allProducts = products;
+          this.products = this.allProducts;
+          console.log(products);
         },
         error: (e) => {
           if(e.error.code === 404) {
@@ -34,6 +38,64 @@ export class ProductsComponent implements OnInit {
       });
     }
     
+  }
+
+  // change the state of the container
+  toggleSelection() { this.isActive = !this.isActive;
+    console.log(this.isActive)
+   }
+
+  loadAllProducts() {
+    this.products = this.allProducts;
+    this.toggleSelection();
+  }
+
+  loadLaptopsAndComputers() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Laptops and Computers';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
+  }
+
+  loadComponentsAndPeripherals() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Components and peripherals';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
+  }
+
+  loadMonitors() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Monitors';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
+  }
+
+  loadServersAndStorages() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Servers and storages';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
+  }
+
+  loadPrinters() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Printers';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
+  }
+
+  loadRouters() {
+    const filteredProducts = this.allProducts?.filter((product) => {
+      return product.productType === 'Routers';
+    });
+    this.products = filteredProducts;
+    this.toggleSelection();
   }
 
 
